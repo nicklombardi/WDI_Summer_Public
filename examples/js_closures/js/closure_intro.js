3cdb3c7bd3c03e209a7ea2d09ae38b73ae4f53df
nicklombardi <nicklombardi@gmail.com>
2013-07-25 13:48:18 -0400

    merging upstream

--- examples/js_closures/js/closure_intro.js
+++ examples/js_closures/js/closure_intro.js
@@ -1,18 +1,19 @@
 function handleStudents(){
   var i = 0;  // scope: local variable, type: number
-  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:???
-  return function(){  // from SO: the inner function has access to the 
-    // environment it was created within 
+  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:number
+  return function(){  // from SO: the inner function has access to the
+    // environment it was created within
     // even after the code that created the environment has returned.
     var student =  students[i]; // s: ???, t: string
-    i+=1; 
-    return student;// What type is this returning? 
+    i+=1;
+    return student;// What type is this returning?
   };
 }
 
 // What type of variable is nextStudent?
 // t: ???
-var nextStudent = handleStudents(); 
+var nextStudent = handleStudents();
+var nextStudent1 = handleStudents();
 
 // onload
 window.onload = function(){
@@ -21,11 +22,15 @@ window.onload = function(){
     document.getElementById('students').innerHTML += nextStudent() + '<br/>\n';
   };
 
+    document.getElementById("addStudent1").onclick = function(){
+    document.getElementById('students').innerHTML += nextStudent1() + '<br/>\n';
+  };
+
   document.getElementById("clearStudents").onclick = function(){
     document.getElementById('students').innerHTML = '';
   };
 
   document.getElementById("resetClosure").onclick = function(){
-    nextStudent = handleStudents(); 
+    nextStudent = handleStudents();
   };
 };
