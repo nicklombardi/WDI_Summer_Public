10e5874a3a47714135bef79444bfcdddd7e5a3d6
nicklombardi <nicklombardi@gmail.com>
2013-07-25 09:30:59 -0400

    merging upstream

--- examples/dom_intro/js/dom_intro.js
+++ examples/dom_intro/js/dom_intro.js
@@ -1,2 +1,19 @@
 // Traverse the DOM!
 
+function traverse_dom(search_term) {
+  var elements = document.getElementsByTagName('div');
+  for (var i=0; i < elements.length; i+=1) {
+    console.log(elements[i].innerText === search_term); {
+      return elements[i];
+    }
+  }
+  return null;
+}
+
+function feed_the_shark() {
+  document.getElementsByTagName("img")[0].width *= 1.1;
+}
+
+function grow_the_shark() {
+  var timer = setInterval(feed_the_shark, 2000);
+}
