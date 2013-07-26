9f705ab9569ea27b4233411b6c821825a5f91019
nicklombardi <nicklombardi@gmail.com>
2013-07-26 11:29:41 -0400

    merging

--- assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
+++ assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
@@ -1,1 +1,16 @@
-# Done in JS? Now do it in Ruby. 
+for i in 1..100
+    div3 = ( i % 3 ).zero?
+    div5 = ( i % 5 ).zero?
+    div3_5 = ( i % 15 ).zero?
+    if div3_5
+      print "FizzBuzz"
+    elsif div3
+      print "Fizz" if div3
+    elsif div5
+      print "Buzz" if div5
+    else
+      print i
+    end
+    puts
+end
+
--- assignments/fizzbuzz/js/fizzbuzz.js
+++ assignments/fizzbuzz/js/fizzbuzz.js
@@ -1,1 +1,14 @@
-// Your code goes here
+var list = _.range(1,101);
+
+var fizzBuzzList = _.map(list, function(number) {
+  if (number % 3 ===0 && number % 5 === 0) {
+    return ("fizzbuzz");
+  } else if (number % 3 === 0 ){
+    return ("fizz");
+  } else if (number % 5 === 0 ){
+    return ("buzz");
+  } else {
+    return (number);
+  }
+});
+
