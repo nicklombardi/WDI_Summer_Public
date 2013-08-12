812bd579c301242a2dede5a3711c6541bf305576
nicklombardi <nicklombardi@gmail.com>
2013-08-12 13:57:10 -0400

    premerge commit

--- assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
+++ assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
@@ -7,4 +7,49 @@ if (typeof Object.create !== 'function') {
 }
 
 var guitar = {
+
+  name: "Fender Esquire",
+  volume: 5,
+  strings: 6,
+
+  setName: function(name) {
+    this.name = name;
+  },
+
+  numberOfStrings: 6,
+  breakString: function() {
+    this.numberOfStrings -= 1;
+  },
+
+  playRock: function() {
+    if (this.numberOfStrings === 6) {
+      this.volume = 11;
+    } else if (this.numberOfStrings === 5) {
+      this.volume = 8;
+    } else if (this.numberOfStrings === 4) {
+      this.volume = 6;
+    } else if (this.numberOfStrings === 3) {
+      this.volume = 4;
+    } else if (this.numberOfStrings === 2) {
+      this.volume = 3;
+    } else if (this.numberOfStrings === 1) {
+      this.volume = 1;
+    } else {
+      this.volume = 0;
+    }
+  },
+
+  restring: function() {
+    this.numberOfStrings = 6;
+    this.volume = 5;
+  },
+
+  playBasicRhythm: function() {
+    return "jug jigga jug jigga jug";
+  },
+
+  playHighStrings: function() {
+    return "meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley MEEEEEEEEEEEEEEEE. \'And the dragon comes in the NIIIiiiiIIIiiiiIIIIIIIIiiiiiiiiiiiIIIIIIIIiiiIIGGHH\'";
+  }
+
 };
